012bc5712e10c7bb6187d6d7ef76781c26e4735e
Evan Jones <evanjones125@gmail.com>
2022-09-20 15:12:11 -0400

    add hover effect for grid

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const grid = document.querySelector('#grid');
 
 for (let i = 0; i < 256; i++) {
     const test = document.createElement('div');
-    test.textContent = `test ${i + 1}`;
     test.classList.add(`${i + 1}`);
     grid.appendChild(test);
 }
+
+// create hover effect
+grid.addEventListener('mouseover', function (e) {
+    e.target.style.background = 'blue';
+});
